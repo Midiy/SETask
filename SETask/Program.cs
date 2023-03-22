e4cf387a82fb097fa3c48013dce9e7725b2325f7
Midiy <midiy009@mail.ru>
2023-03-22 17:22:30 +0300

    More verbose output

--- SETask/Program.cs
+++ SETask/Program.cs
@@ -3,8 +3,11 @@
 List<Func<int>> counterProviders = new();
 
 for (int i = 0; i < 10; i++)
+{
     counterProviders.Add(() => i * 3);
+    Console.WriteLine($"From 'for': {i}.");
+}
 
 IEnumerable<string> outputStrings = counterProviders.Select(provider => provider?.Invoke().ToString("X") ?? string.Empty);
 foreach (string outputString in outputStrings)
-    Console.WriteLine(outputString);
+    Console.WriteLine($"From lambdas: {outputString}.");
