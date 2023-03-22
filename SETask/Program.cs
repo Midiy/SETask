80711a7a71cb458af7bca4f5c7e1d024f34e99cf
Midiy <midiy009@mail.ru>
2023-03-22 17:20:03 +0300

    Lambdas demo

--- SETask/Program.cs
+++ SETask/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<Func<int>> counterProviders = new();
+
+for (int i = 0; i < 10; i++)
+    counterProviders.Add(() => i);
+
+IEnumerable<string> outputStrings = counterProviders.Select(provider => provider?.Invoke().ToString("X") ?? string.Empty);
+foreach (string outputString in outputStrings)
+    Console.WriteLine(outputString);
